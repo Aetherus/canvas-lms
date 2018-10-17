d74a88789c2d3702448113851a285837642b6d57
zhouminhao <zhouminhao@ziyuenet.com.cn>
2018-10-17 11:26:25 +0800

    Include development gems for production hosts for compile assets

--- Gemfile.d/capistrano.rb
+++ Gemfile.d/capistrano.rb
@@ -1,4 +1,4 @@
-group :development do
+group :deployment do
   gem "capistrano", "~> 3.10", require: false
   gem "capistrano-rails", "~> 1.3", require: false
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ lock "~> 3.11.0"
 set :application, "canvas-lms"
 set :repo_url, "ssh://zhouminhao@172.168.70.223:29418/lms-canvas.git"
 
+set :rails_env, :production
+
 # Defaults to [:web]
 # set :assets_roles, [:web]
 
@@ -38,10 +40,18 @@ append :linked_files, *%w[
 ]
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "tmp/files"
+append :linked_dirs, ".bundle", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "tmp/files"
 
 # Default value for default_env is {}
 set :default_env, { path: "/opt/ruby/bin:$PATH", rails_env: 'production' }
+
+# Default value for :bundle_without is 'development test'
+# Because some gems for building assets are installed only in development environment,
+# we have to install them on the target hosts.
+# We added capistrano related gems in the group 'deployment',
+# and these gems are only needed on the development host,
+# so we can exclude them on the target hosts
+set :bundle_without, 'test deployment'
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
