455ffb008130a4e40fd34d6edbf5f1b01bdf5679
zhouminhao <zhouminhao@ziyuenet.com.cn>
2019-02-01 12:36:00 +0800

    create table announcements

---
+++ app/models/course_search.rb
@@ -0,0 +1,3 @@
+class CourseSearch < ApplicationRecord
+  belongs_to :course
+end
---
+++ db/migrate/20190201011816_create_announcements.rb
@@ -0,0 +1,10 @@
+class CreateAnnouncements < ActiveRecord::Migration[5.1]
+  tag :postdeploy
+
+  def change
+    create_table :announcements do |t|
+      t.string :title
+      t.text :content
+    end
+  end
+end
